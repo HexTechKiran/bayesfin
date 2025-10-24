e28cc901f2af6f10e915f2da6c56bedffcece8dc
kiran <kiranjorgensen@gmail.com>
2025-10-24 18:13:20 -0400

    SBI for volatility coefficients in a GBM model

--- Vol_test.py
+++ Vol_test.py
@@ -1,3 +1,10 @@
+"""
+Simulates a multivariable geometric brownian motion with 3 variables, and performs inference on
+the volatility vector used as an input to create the volatility coefficient matrix.
+Outputs metrics on the posterior distributions generated.
+"""
+
+
 import os
 
 if "KERAS_BACKEND" not in os.environ:
@@ -14,22 +21,28 @@ import os
 import corner
 import jax
 
-RNG = np.random.default_rng(2025)
-DRIFT_SCALE = 1
+RNG = np.random.default_rng(int(os.times()[4]))
+VOL_MEAN = 0.24621856131518247
+VOL_STDEV = 0.0049087692859631936
 
 def prior():
     # Generates a random draw from the prior
 
-    b1 = RNG.uniform(-DRIFT_SCALE, DRIFT_SCALE)
-    b2 = RNG.uniform(-DRIFT_SCALE, DRIFT_SCALE)
-    b3 = RNG.uniform(-DRIFT_SCALE, DRIFT_SCALE)
+    v1 = RNG.lognormal(mean=VOL_MEAN, sigma=VOL_STDEV)
+    v2 = RNG.lognormal(mean=VOL_MEAN, sigma=VOL_STDEV)
+    v3 = RNG.lognormal(mean=VOL_MEAN, sigma=VOL_STDEV)
 
-    return {"b1":b1, "b2":b2, "b3":b3}
+    return {"v1":v1, "v2":v2, "v3":v3}
 
-def GBM_sim(b1, b2, b3, x0 = np.array([100, 100, 100]), time = 100/365, time_step = 1/365):
-    sigma = np.array([[0.5, 0.1, 0.0],
-                      [0.0, 0.1, 0.3],
-                      [0.0, 0.0, 0.2]])
+def GBM_sim(v1, v2, v3, x0 = np.array([100, 100, 100]), time = 100/365, time_step = 1/365):
+    stdevs = np.array([v1, v2, v3])
+    stdevs_D = np.diag(stdevs)
+    correlation = np.array([[1.0, 0.4472136, 0.0],
+                            [0.0, 1.0, 2.12132],
+                            [0.0, 0.0, 1.0]])
+    sigma = np.dot(stdevs_D, correlation, stdevs_D)
+
+    b1, b2, b3 = 0.2, 0.4, -0.3
 
     x = x0
 
@@ -65,7 +78,7 @@ if __name__ == "__main__":
     prior_sample = prior()
     time_ = 100/365
     time_step_ = 1/365
-    motion = GBM_sim(prior_sample["b1"], prior_sample["b2"], prior_sample["b3"])["motion"]
+    motion = GBM_sim(prior_sample["v1"], prior_sample["v2"], prior_sample["v3"])["motion"]
 
     plt.plot(np.arange(0, time_, time_step_), [row[0] for row in motion])
     plt.plot(np.arange(0, time_, time_step_), [row[1] for row in motion])
@@ -79,14 +92,14 @@ if __name__ == "__main__":
         bf.adapters.Adapter()
         .convert_dtype("float64", "float32")
         .as_time_series("motion")
-        .concatenate(["b1", "b2", "b3"], into="inference_variables")
+        .concatenate(["v1", "v2", "v3"], into="inference_variables")
         .rename("motion", "summary_variables")
         .log(["inference_variables", "summary_variables"], p1=True)
     )
 
-    summary_net = GRU()
+    summary_net = bf.networks.TimeSeriesNetwork(dropout=0.1)
 
-    inference_net = bf.networks.CouplingFlow()
+    inference_net = bf.networks.CouplingFlow(transform="spline", depth=2, dropout=0.1)
 
     workflow = bf.BasicWorkflow(
         simulator=simulator,
@@ -96,11 +109,11 @@ if __name__ == "__main__":
         standardize=None
     )
 
-    train = workflow.simulate(10000)
-    validation = workflow.simulate(500)
+    train = workflow.simulate(8000)
+    validation = workflow.simulate(300)
 
     history = workflow.fit_offline(data=train,
-                                   epochs=300,
+                                   epochs=100,
                                    batch_size=32,
                                    validation_data=validation)
 
@@ -119,17 +132,17 @@ if __name__ == "__main__":
 
     f = bf.diagnostics.plots.recovery(samples, test_sims)
 
-    b1_truth = test_sims["b1"][0].item()
-    b2_truth = test_sims["b2"][0].item()
-    b3_truth = test_sims["b3"][0].item()
+    b1_truth = test_sims["v1"][0].item()
+    b2_truth = test_sims["v2"][0].item()
+    b3_truth = test_sims["v3"][0].item()
     truths = np.asarray([b1_truth, b2_truth, b3_truth])
 
-    b1_samples = samples["b1"][0].flatten()
-    b2_samples = samples["b2"][0].flatten()
-    b3_samples = samples["b3"][0].flatten()
+    b1_samples = samples["v1"][0].flatten()
+    b2_samples = samples["v2"][0].flatten()
+    b3_samples = samples["v3"][0].flatten()
     out_samples = np.asarray([b1_samples, b2_samples, b3_samples]).T
 
-    labels = ["b1", "b2", "b3"]
+    labels = ["v1", "v2", "v3"]
 
     d = out_samples.shape[1]
     fig, axes = plt.subplots(d, d, figsize=(8, 8))
